0f5913ab4ac807418638f14bb09804f2803415f8
hkasparian <hayk.kasparian@gmail.com>
2015-06-03 01:39:07 -0700

    GSAP/Tween Animation work, CSS tweaks, content
    smh pretty sure my site is dead because it's non-responsive af.
    
    Also considering possibility I may be colorblind. TBD.

--- js/kobe.js
+++ js/kobe.js
@@ -42,7 +42,7 @@ $(window).load(function() {
 $(window).resize(function() {
 
     // Kill for small screens
-    if ($(window).width() <= 768 && !scrollMagicIsDestroyed) {
+    if ($(window).width() <= 200 && !scrollMagicIsDestroyed) {
         destroyScrollMagic();
         scrollMagicIsDestroyed = true;
         $('body').addClass('no-scroll-magic');
@@ -59,8 +59,8 @@ $(window).resize(function() {
 ========================================*/
 function createScrollMagic() {
 
-    $('section').each(function() {
-        var cardTween = TweenMax.fromTo('#'+$(this).attr('id')+' .wrap', 1,
+    $('section:odd').each(function() {
+        var cardTweenOdd = TweenMax.fromTo('#'+$(this).attr('id')+' .wrap', 1,
             {
                 scale: 0,
                 // z: 1,
@@ -86,37 +86,69 @@ function createScrollMagic() {
             offset: -250
         })
         .addTo(scrollMagicController)
-        .setTween(cardTween);
+        .setTween(cardTweenOdd);
 
-        // Begin Signature Tween
 
-        function pathPrepare ($el) {
-            var lineLength = $el[0].getTotalLength();
-            $el.css("stroke-dasharray", lineLength);
-            $el.css("stroke-dashoffset", lineLength);
-        }
-
-        var $letterH = $("path#letterH");
-        var $ayk = $("path#ayk");
-        var $kasparian = $("path#kasparian");
-
-        // prepare SVG
-        pathPrepare($letterH);
-        pathPrepare($ayk);
-        pathPrepare($kasparian);
-
-        // build tween
-        var tween = new TimelineMax()
-            .add(TweenMax.to($letterH, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-            .add(TweenMax.to($ayk, 1.2, { strokeDashoffset: 0, ease: Linear.easeNone }))  // draw dot for 0.4
-            .add(TweenMax.to($kasparian, 1, { strokeDashoffset: 0, ease: Linear.easeNone }))  // draw dot for 1
-            .add(TweenMax.to("path", 1, {stroke: "#FFF", ease:Linear.easeNone}), 0);			// change color during the animation
-
-        // build scene
-        var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 250, tweenChanges: true})
-            .setTween(tween)
-            .addTo(scrollMagicController);
     });
+    $('section:even').each(function () {
+        var cardTweenEven = TweenMax.fromTo('#' + $(this).attr('id') + ' .wrap', 1,
+            {
+                scale: 0,
+                // z: 1,
+                y: 500,
+                x: -800,
+                skewX: '-200deg',
+                transformPerspective: -200,
+                rotation: '10deg'
+            },
+            {
+                scale: 1,
+                // z: 1,
+                y: 0,
+                x: 0,
+                skewX: 0,
+                rotation: 0
+            }
+        );
+
+        var cardScene = new ScrollMagic.Scene({
+            triggerElement: '#' + $(this).attr('id'),
+            duration: $(window).height() * 0.7,
+            offset: -250
+        })
+        .addTo(scrollMagicController)
+        .setTween(cardTweenEven);
+
+
+    });
+    // Begin Signature Tween
+
+    function pathPrepare($el) {
+        var lineLength = $el[0].getTotalLength();
+        $el.css("stroke-dasharray", lineLength);
+        $el.css("stroke-dashoffset", lineLength);
+    }
+
+    var $letterH = $("path#letterH");
+    var $ayk = $("path#ayk");
+    var $kasparian = $("path#kasparian");
+
+    // prepare SVG
+    pathPrepare($letterH);
+    pathPrepare($ayk);
+    pathPrepare($kasparian);
+
+    // build tween
+    var tween = new TimelineMax()
+        .add(TweenMax.to($letterH, 0.5, { strokeDashoffset: 0, ease: Linear.easeNone })) // draw word for 0.9
+        .add(TweenMax.to($ayk, 1.2, { strokeDashoffset: 0, ease: Linear.easeNone }))  // draw dot for 0.4
+        .add(TweenMax.to($kasparian, 1, { strokeDashoffset: 0, ease: Linear.easeNone }))  // draw dot for 1
+        .add(TweenMax.to("path", 1, { stroke: "#FFF", ease: Linear.easeNone }), 0);			// change color during the animation
+
+    // build scene
+    var scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 250, tweenChanges: true })
+        .setTween(tween)
+        .addTo(scrollMagicController);
 }
 
 function destroyScrollMagic() {
